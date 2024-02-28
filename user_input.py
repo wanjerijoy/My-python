dee30d0b77f9079b0e39e8ccfb402b8fce8f8544
JOY WANJERI <wanjerijoy8@gmail.com>
2024-02-28 00:26:42 +0000

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name=input("Enter your name:")
+name
+age=input("Enter your age:")
+age
+location=input("Enter your location:")
+location
+print(f"Hello {name},you are {age} years old and live in {location}")
